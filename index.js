8aa417aae7b92334a3de74859eb81aeeb842d1bf
Terry Foley <terry.foley555@gmail.com>
2018-06-15 04:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,5 @@ function takeANumber(line, name){
   return "Welcome, " + name + ". You are number " + line.length +
   " in line."
 }
-takeANumber(katzDeliLine, "Ada")
 
-function nowServing(line, name){
-line.push(name);
-  if (line.length < 2){
-    return "There is nobody waiting to be served!";
-  }
-  else {
-    return "Currently serving " + name + ".";
-  }
-}
+takeANumber(katzDeliLine, "Ada")
