126eb523a04189f0883c4ccbeae33523c60f2581
Terry Foley <terry.foley555@gmail.com>
2018-06-15 05:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function nowServing(line){
     return "There is nobody waiting to be served!";
   }
   else {
-    return "Currently serving " + name ".";
+    return "Currently serving " + name + ".";
   }
 }
