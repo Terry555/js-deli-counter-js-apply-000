797622f03cac61371f5055cf5cd0bbea88bd4b0f
Terry Foley <terry.foley555@gmail.com>
2018-06-15 04:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var katzDeli = [];
 
 function takeANumber(line, name){
   line.push(name);
   return "Welcome, " + name + ". You are number " + line.length +
   " in line."
 }
-takeANumber(katzDeli, "Ada")
+takeANumber(katzDeliLine, "Ada")
+
+function nowServing(line, name){
+line.push(name);
+  if (line.length < 2){
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    return "Currently serving " + name + ".";
+  }
+}
